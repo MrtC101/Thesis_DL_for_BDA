08aa7d0884e6399fbbff05196907a4c28a283617
Mrtc101 <martincogo4@gmail.com>
2024-05-16 18:03:44 -0300

    Documented prepare_folder

--- src/preprocessing/prepare_folder/delete_extra.py
+++ src/preprocessing/prepare_folder/delete_extra.py
@@ -1,48 +1,62 @@
 import os
 import sys
-if (os.environ.get("SRC_PATH") not in sys.path):
-    sys.path.append(os.environ.get("SRC_PATH"))
-from utils.common.logger import get_logger
-l = get_logger("delete_extra")
-
 import random
 import argparse
 from tqdm import tqdm
 from itertools import chain
 from utils.pathManagers.rawManager import RawPathManager
 
-def leave_only_n(data_path : dict, n : int) -> None:
+if (os.environ.get("SRC_PATH") not in sys.path):
+    sys.path.append(os.environ.get("SRC_PATH"))
+from utils.common.logger import LoggerSingleton, TqdmToLog
+log = LoggerSingleton()
+
+
+def leave_only_n(data_path: dict, n: int) -> None:
+    """Pick n random disaster tiles and deletes all the rest of files from
+    disaster Dataset.
+
+    Args:
+        data_path: Path to the directory to the xBD dataset that contains
+        subsets of xBD dataset. 
+        n: number of disasters that will be left in the folder.
+
+    Example:
+        >>> leave_only_n("data/xBD/raw",45)
     """
-    Pick n random tiles and deletes all the rest of files from disaster Dataset.
-    """
+
+    log.name = "Delete Extra"
     data = RawPathManager.load_paths(data_path)
 
     tiles = list(chain.from_iterable(data))
     total_tiles = len(tiles)
-    if(total_tiles>n):
-        l.info(f"Deleting {n} tiles of {total_tiles} total tiles.")
+    if (total_tiles > n):
+        log.info(f"Deleting {n} tiles of {total_tiles} total tiles.")
 
-        ids = [random.randint(0, total_tiles) for _ in range(n)]    
+        ids = [random.randint(0, total_tiles) for _ in range(n)]
 
         files_to_remove = list(chain.from_iterable(chain.from_iterable(tiles)))
-        l.info(f"{len(files_to_remove)} files to remove.")
-        for i in tqdm(range(total_tiles)):
-            if(i not in ids):
+        log.info(f"{len(files_to_remove)} files to remove.")
+        for i in tqdm(range(total_tiles), file=TqdmToLog(log)):
+            if (i not in ids):
                 for file in files_to_remove:
                     os.remove(file)
-        l.info("Files removed.")
+        log.info("Files removed.")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Pick n random tiles and deletes all the rest of files from disaster Dataset.')
+        description="Pick n random disaster's tiles and deletes all the rest \
+            of files from disaster Dataset.")
     parser.add_argument(
         'data_path',
-        help=('Path to xBD dataset folder.')
+        help=("Path to xBD dataset folder that contains subsets. \
+              Ex: data/xBD/raw")
     )
     parser.add_argument(
         '-n', '--only_n',
         type=int,
-        help=('Only leaves n tiles.')
+        help=('Only leaves n disaster tiles.')
     )
     args = parser.parse_args()
-    leave_only_n(args.data_path,args.n)
+    leave_only_n(args.data_path, args.n)
--- src/train_pipeline.py
+++ src/train_pipeline.py
@@ -1,50 +1,59 @@
+"""Train Pipeline
+
+This module establishes the sequence of execution of
+scripts inside `preprocessing`, `train` and `test`
+packages, to implement the data preprocessing,
+model training and testing pipeline for this project.
+"""
 import os
 import sys
-
-os.environ["PROJ_PATH"] = "/home/mrtc101/Desktop/tesina/repo/my_siames"
-os.environ["SRC_PATH"] = os.path.join(os.environ["PROJ_PATH"],"src")
-os.environ["DATA_PATH"] = os.path.join(os.environ["PROJ_PATH"],"data")
-
-if (os.environ.get("SRC_PATH") not in sys.path):
-    sys.path.append(os.environ.get("SRC_PATH"))
-from utils.common.logger import LoggerSingleton    
-
-from preprocessing.sliced.split_sliced_dataset import split_sliced_dataset
+from os.path import join
+from utils.common.logger import LoggerSingleton
 from preprocessing.prepare_folder.clean_folder import delete_not_in
-from preprocessing.prepare_folder.delete_extra import leave_only_n
 from preprocessing.prepare_folder.create_label_masks import create_masks
+from preprocessing.prepare_folder.delete_extra import leave_only_n
 from preprocessing.raw.data_stdv_mean import create_data_dicts
 from preprocessing.raw.split_raw_dataset import split_dataset
 from preprocessing.sliced.make_smaller_tiles import slice_dataset
 from preprocessing.sliced.split_sliced_dataset import split_sliced_dataset
-from preprocessing.shards.make_data_shards import create_shards
 from preprocessing.shards.split_shard_dataset import split_shard_dataset
+from preprocessing.shards.make_data_shards import create_shards
 from train.training import train_model
 
+os.environ["PROJ_PATH"] = "/home/mrtc101/Desktop/tesina/repo/my_siames"
+os.environ["SRC_PATH"] = join(os.environ["PROJ_PATH"], "src")
+os.environ["DATA_PATH"] = join(os.environ["PROJ_PATH"], "data")
+os.environ["OUT_PATH"] = join(os.environ["PROJ_PATH"], "out")
+
+if (os.environ.get("SRC_PATH") not in sys.path):
+    sys.path.append(os.environ.get("SRC_PATH"))
 
 def preprocess():
-    xbd_path = os.path.join(os.environ["DATA_PATH"],"xBD")
-    raw_path = os.path.join(xbd_path,"raw")
+    """Pipeline sequence for data preprocessing."""
+    xbd_path = join(os.environ["DATA_PATH"], "xBD")
+    raw_path = join(xbd_path, "raw")
     # folder cleaning
     delete_not_in(raw_path)
-    create_masks(raw_path,1)
-    leave_only_n(raw_path,40)
+    create_masks(raw_path, 1)
+    leave_only_n(raw_path, 40)
     # Raw data
-    split_json_path = split_dataset(raw_path,xbd_path)
-    data_dicts_path = create_data_dicts(split_json_path,xbd_path)
+    split_json_path = split_dataset(raw_path, xbd_path)
+    data_dicts_path = create_data_dicts(split_json_path, xbd_path)
     # Sliced
-    sliced_path = os.path.join(xbd_path,"sliced")
-    slice_dataset(split_json_path,sliced_path)
-    split_sliced_json_path = split_sliced_dataset(sliced_path,split_json_path,xbd_path)
+    sliced_path = join(xbd_path, "sliced")
+    slice_dataset(split_json_path, sliced_path)
+    split_sliced_json_path = split_sliced_dataset(
+        sliced_path, split_json_path, xbd_path)
     # Sharded
-    mean_stddev_json = os.path.join(data_dicts_path,"all_tiles_mean_stdev.json")
-    shards_path = os.path.join(xbd_path,"shards")
-    create_shards(split_sliced_json_path,mean_stddev_json,shards_path,4)
-    split_shard_json_path = split_shard_dataset(shards_path,xbd_path)
+    mean_stddev_json = join(
+        data_dicts_path, "all_tiles_mean_stdev.json")
+    shards_path = join(xbd_path, "shards")
+    create_shards(split_sliced_json_path, mean_stddev_json, shards_path, 4)
+    split_shard_json_path = split_shard_dataset(shards_path, xbd_path)
     return split_shard_json_path
 
-# train
 def train(split_shard_json_path):
+    """Pipeline sequence for training the model."""
     train_config = {
         'labels_dmg': [0, 1, 2, 3, 4],
         'labels_bld': [0, 1],
@@ -52,27 +61,31 @@ def train(split_shard_json_path):
         'weights_damage': [1, 35, 70, 150, 120],
         'weights_loss': [0, 0, 1],
         'mode': 'dmg',
-        'init_learning_rate': 0.0005,#dmg: 0.005, #UNet: 0.01,           
+        'init_learning_rate': 0.0005,  # dmg: 0.005, #UNet: 0.01,
         'device': 'cpu',
-        'epochs': 2,#1500,
+        'epochs': 2,  # 1500,
         'batch_size': 32,
         'num_chips_to_viz': 1
     }
-    out_dir = os.path.join(os.environ["PROJ_PATH"],"out")
     path_config = {
         'exp_name': 'train_UNet',  # train_dmg
-        'out_dir': out_dir,
+        'out_dir': os.environ["OUT_PATH"],
         'shard_splits_json': split_shard_json_path,
-        'label_map_json': '/home/mrtc101/Desktop/tesina/repo/my_siames/data/constants/xBD_label_map.json',
+        'label_map_json': join(os.environ["DATA_PATH"], "constants",
+                               "xBD_label_map.json"),
         'starting_checkpoint_path': None
     }
-    train_model(train_config,path_config)
+    train_model(train_config, path_config)
 
 # evaluate with test
 # test_model()
 # inference
 
+
 if __name__ == "__main__":
-    l = LoggerSingleton("Training Pipeline","/home/mrtc101/Desktop/tesina/repo/my_siames/out/console_logs") # FIRST AND UNIQUE LOGGER FROM ALL TRAINING PIPELINE
-    data = preprocess()
-    train("/home/mrtc101/Desktop/tesina/repo/my_siames/data/xBD/splits/shard_splits.json")
+    # FIRST AND UNIQUE LOGGER FROM ALL TRAINING PIPELINE
+    log = LoggerSingleton("Training Pipeline",
+                          join(os.environ["OUT_PATH"], "console_logs"))
+    # split_shard_json_path = preprocess()
+    split_shard_json_path = join(os.environ["DATA_PATH"], "xBD", "shards")
+    train(split_shard_json_path)
