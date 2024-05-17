6759dbf7a229c98a32abaf1589b94a8d419c46f4
Mrtc101 <martincogo4@gmail.com>
2024-05-16 22:18:08 -0300

    common utils Documented

--- src/utils/common/defaultDictFactory.py
+++ src/utils/common/defaultDictFactory.py
@@ -1,17 +1,20 @@
 from collections import defaultdict
 
+
 def nested_defaultdict(depth, default_factory=dict):
     """
     Crea un defaultdict anidado con la profundidad especificada.
-    
+
     Args:
         depth (int): Profundidad del anidamiento.
-        default_factory (callable, opcional): Función para inicializar los defaultdicts. Por defecto es dict.
-    
+        default_factory (callable, opcional): Función para inicializar los
+        defaultdict. Por defecto es dict.
+
     Returns:
         defaultdict: defaultdict anidado con la profundidad especificada.
     """
     if depth <= 1:
         return defaultdict(default_factory)
     else:
-        return defaultdict(lambda: nested_defaultdict(depth - 1, default_factory))
+        return defaultdict(
+            lambda: nested_defaultdict(depth - 1, default_factory))
--- src/utils/common/logger.py
+++ src/utils/common/logger.py
@@ -2,19 +2,18 @@ from io import TextIOWrapper
 import os
 import logging
 import sys
-
 from tqdm import tqdm
-from tqdm.contrib import DummyTqdmFile
-
-#TODO primero se crea un singleton de un logger
-#TODO despues se crea un handler para ese logger que utiliza la función write de tqdm para escribir en consola y en archivo
-#TODO como se quiere registrar los progresos tqdm se hizo la función TqdmToLog que pasa la barra a el log que luego escribe con tqdm que luego escribe en el archivo de salida
-    
-
 from utils.common.files import is_dir
 
-class TdqmToLog:
-    def __init__(self, log : logging.Logger):
+
+class TqdmToLog:
+    """
+    If TqdmToLog is used, the progress bar is printed into a logger,
+    braking the tqdm console normal behavior, but allows the tqdm()
+    progress bar to be dumpt into a logger file.
+    """
+
+    def __init__(self, log: logging.Logger):
         self.log = log
 
     def write(self, message):
@@ -23,8 +22,17 @@ class TdqmToLog:
     def flush(self):
         pass
 
-class LoggerSingleton:
 
+class LoggerSingleton:
+    """
+    This module defines a singleton class for a logger. The pipeline utilizes
+    only this class' logger, which is modified at the start of each step
+    of the pipeline.
+    It is possible to change the `name` and the `file handler`
+    by simply calling the class constructor with the new arguments.
+    This behavior is desirable because all log messages are dumped into one
+    file until it is intentionally changed.
+    """
     _instance: logging.Logger = None
     _last_file_handler = None
     _level = logging.INFO  # Nivel de registro predeterminado
@@ -33,9 +41,9 @@ class LoggerSingleton:
         datefmt='%m-%d-%Y %H:%M',
     )
 
-    def new_tqdm_handler(cls,folder_path):
+    def new_tqdm_handler(cls, folder_path):
         file_name = cls._instance.name.replace(" ", "_").strip("")
-        file = os.path.join(folder_path, f"{file_name}.txt")    
+        file = os.path.join(folder_path, f"{file_name}.txt")
         tqdm_handler = TqdmLoggingHandler(filename=file, mode='w')
         tqdm_handler.setLevel(cls._level)
         tqdm_handler.setFormatter(cls._formatter)
@@ -46,8 +54,8 @@ class LoggerSingleton:
         stream_handler.setLevel(cls._level)
         stream_handler.setFormatter(cls._formatter)
         cls._instance.addHandler(stream_handler)
-    
-    def new_file_handler(cls,folder_path):
+
+    def new_file_handler(cls, folder_path):
         is_dir(folder_path)
         file_name = cls._instance.name.replace(" ", "_").strip("")
         file = os.path.join(folder_path, f"{file_name}.txt")
@@ -56,27 +64,34 @@ class LoggerSingleton:
         file_handler.setFormatter(cls._formatter)
         cls._instance.addHandler(file_handler)
         cls._instance._last_file_handler = file_handler
-        
+
     def __new__(cls, name=None, level=None, folder_path=None):
         if cls._instance is None:
             cls._instance = logging.getLogger(name)
-            cls._instance.setLevel(level or cls._level) 
-            #cls.new_console_handler(cls)
+            cls._instance.setLevel(level or cls._level)
+            # cls.new_console_handler(cls)
         if name is not None:
             cls._instance.name = name
         if folder_path is not None:
             if cls._last_file_handler is not None:
                 cls._instance.removeHandler(cls._last_file_handler)
-            #cls.new_file_handler(cls,folder_path=folder_path)
-            cls.new_tqdm_handler(cls,folder_path=folder_path)
+            # cls.new_file_handler(cls,folder_path=folder_path)
+            cls.new_tqdm_handler(cls, folder_path=folder_path)
         return cls._instance
 
-class TqdmLoggingHandler(logging.Handler):
 
-    def __init__(self,filename,mode):
+class TqdmLoggingHandler(logging.Handler):
+    """
+    The logger class uses only one handler, "TqdmLoggingHandler". This handler
+    utilizes the method tqdm.write() for writing into both the standard output
+    and the output file. By doing this, you can view each progress with logger
+    messages without any problems throw the console.
+"""
+
+    def __init__(self, filename, mode):
         super().__init__()
         self.filename = filename
-        self.f :TextIOWrapper = open(filename,mode)
+        self.f: TextIOWrapper = open(filename, mode)
         self.mode = mode
 
     def emit(self, record):
