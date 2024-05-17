90033fcddc501520507ef7e585ec68534bbe8f1b
Mrtc101 <martincogo4@gmail.com>
2024-05-17 01:27:05 -0300

    Cleaning docker files

--- src/train_pipeline.py
+++ src/train_pipeline.py
@@ -8,10 +8,12 @@ model training and testing pipeline for this project.
 import os
 from os.path import join
 import sys
+# Paths to change in production
 os.environ["PROJ_PATH"] = "/home/mrtc101/Desktop/tesina/repo/my_siames"
 os.environ["SRC_PATH"] = join(os.environ["PROJ_PATH"], "src")
 os.environ["DATA_PATH"] = join(os.environ["PROJ_PATH"], "data")
 os.environ["OUT_PATH"] = join(os.environ["PROJ_PATH"], "out")
+
 if (os.environ.get("SRC_PATH") not in sys.path):
     sys.path.append(os.environ.get("SRC_PATH"))
 from utils.common.logger import LoggerSingleton
