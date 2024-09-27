3e083718e5a740136dc4fd47a13e6d141b54e747
Mrtc101 <martincogo4@gmail.com>
2024-09-27 04:14:32 -0300

    New web page style

--- web_page/static/js/index.js
+++ web_page/static/js/index.js
@@ -3,3 +3,14 @@ function changeLanguage(lang) {
   next_lang = curr_lang == "en" ? "es" : "en"
   window.location.replace(`/${next_lang}`);
 }
+
+// Change theme listener
+(() => {
+  const themeToggleButton = document.getElementById('theme-switcher');
+  themeToggleButton.addEventListener('click', () => {
+    const currentTheme = getStoredTheme();
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark'; // Alternar entre temas
+    setStoredTheme(newTheme);
+    setTheme(newTheme);
+  });
+})();
